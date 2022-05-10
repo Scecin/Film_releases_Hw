a6cd8bee7a04a92d5b2c182e3ab75e1726754aec
Scecin <silvia.cecin@hotmail.com>
2022-05-10 18:59:07 +0100

    First commit

---
+++ src/components/FilmButton.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FilmButton = () => {
+    return (
+        <a href="https://www.imdb.com/calendar/?region=gb">
+            <button>View more upcoming releases >></button>
+        </a>
+    )
+}
+
+export default FilmButton;
---
+++ src/components/FilmList.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Film from "./Film";
+
+const FilmList = ({films}) => {
+
+    const filmNodes = films.map  ((film) => {
+        return (
+            <Film key={film.id} name = {film.name}  url = {film.url}/>
+        )
+    })
+
+    return (
+        <>
+            {filmNodes}
+        </>
+    )
+}
+
+export default FilmList;
